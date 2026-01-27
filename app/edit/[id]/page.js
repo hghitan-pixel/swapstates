5a027e218a348c78e30b98819ea250caa92034fd
hghitan-pixel <hghitan@gmail.com>
2026-01-27 09:15:29 -0500

    Update page.js

--- app/edit/[id]/page.js
+++ app/edit/[id]/page.js
@@ -22,6 +22,7 @@ function Navigation() {
           <div className="hidden md:flex items-center space-x-6">
             <Link href="/" className="hover:text-blue-200">Home</Link>
             <Link href="/browse" className="hover:text-blue-200">Browse</Link>
+    <Link href="/about" className="hover:text-blue-200">Who We Are</Link>
             <Link href="/list" className="bg-white text-blue-800 px-4 py-2 rounded-lg font-semibold">List Your Home</Link>
           </div>
           <button onClick={() => setMenuOpen(!menuOpen)} className="md:hidden p-2">
